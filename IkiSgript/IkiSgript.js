20af7ec2c345109141a0efc24b99e7d70a4c86d1
yutorin1001 <163138241+yutorin1001@users.noreply.github.com>
2024-03-12 21:33:34 +0900

    Add files via upload

---
+++ IkiSgript/IkiSgript.js
@@ -0,0 +1,17 @@
+
+var a = document.getElementById("IKISUGI").innerText;
+
+
+
+let newstr = a.replace(/あくしろよ/g, /setTimeout/g).replace(/まずいですよ!/g, 'alert').replace(/金！暴力！SEX！/g,'log')
+.replace(/今日は、アドルフ・アイヒマンが逮捕された日なんですよ/g,'history').replace(/ココアライオン/g, 'addEventListener').replace('窓際行って', 'window')
+.replace(/これもうわかんねぇな/g, 'random').replace(/金！暴力！SEX！/g,'log')
+.replace(/咥えて差し上げろ/g, 'write').replace(/アゼルバイジャン/g, 'var').replace(/多分変態だと思うんですけど/g,'if')
+.replace(/24歳、学生です/g, 'Math').replace(/ヌッ！/g, 'break').replace(/ファッ！？/g, 'case').replace(/ブッチッパ！/g,'switch')
+.replace(/王道を往く/g, 'console').replace(/こ↑こ↓/g, 'document').replace('(≧Д≦)','function').replace(/開いてんじゃーん/g,'open')
+.replace(/デデドン！/g,'erro').replace(/見とけよ見とけよー/g,'getElementById').replace(/そのための右手/g, 'right').replace(/いいんだ上等だろ/g,'top')
+.replace(/情けない格好/g,'style').replace(/引っ越ししちゃいかんのか/g,'innerText')
+.replace(/ホモなんだろ？/g,'parseInt').replace(/おかのした/g,'id').replace(/じゃけん/g,'||').replace(/そうだよ/g,'&&');
+
+
+eval(newstr); 
